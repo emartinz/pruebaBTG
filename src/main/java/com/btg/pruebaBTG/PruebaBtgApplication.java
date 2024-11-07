164b1a959c4883247d7c7bd320a58dce086c5a88
Edgar Martinez <edgar.a.mz@gmail.com>
2024-11-07 10:41:48 -0500

    ProyectoBase

---
+++ src/main/java/com/btg/pruebaBTG/PruebaBtgApplication.java
@@ -0,0 +1,13 @@
+package com.btg.pruebaBTG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btg/pruebaBTG/PruebaBtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.pruebaBTG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
